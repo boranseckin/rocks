94dd1eeb0a2998a0d3c997d5e2134747b242bce7
Boran Seckin <boran@boranseckin.com>
2023-05-21 11:05:47 -0400

    tests: opt in to local stdout testing

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -3,17 +3,17 @@ macro_rules! tests {
     ($file:ident in $scope:ident is OK $($expected:expr)+) => {
         #[test]
         fn $file() {
-            use assert_cmd::Command;
-            // output concats expected with new line
-            let output = vec![$($expected),+].join("\n");
-            let header = format!("rocks v{}", env!("CARGO_PKG_VERSION"));
-            let file = format!("examples/{}/{}.rocks", stringify!($scope), stringify!($file));
+            use rocks_lang::rocks;
 
-            Command::cargo_bin("rocks").unwrap()
-                .arg(file)
-                .assert()
-                .stdout(format!("{header}\n{output}\n"))
-                .success();
+            let expected = vec![$($expected),+, ""].join("\n");
+
+            let mut output = Vec::new();
+            let mut rocks = rocks::new(&mut output);
+
+            rocks.run_file(format!("examples/{}/{}.rocks", stringify!($scope), stringify!($file)));
+
+            drop(rocks);
+            assert_eq!(expected, std::str::from_utf8(&output).unwrap());
         }
     };
 
