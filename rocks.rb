c6b62d328f8b1f33f0c2420dd67b70b25db25669
Boran Seckin <boran@boranseckin.com>
2023-06-29 23:42:59 -0400

    chore: bump version to v0.2.2

--- rocks.rb
+++ rocks.rb
@@ -2,8 +2,8 @@ class Rocks < Formula
   desc "Interpreter for rocks-lang"
   homepage "https://github.com/boranseckin/rocks"
   url "https://github.com/boranseckin/rocks/releases/latest/download/rocks.tar.gz"
-  version "0.2.1"
-  sha256 "25cf524709bf7d562dba84fe727c9ea994a73c1877121f966e304b676f778c12"
+  version "0.2.2"
+  sha256 "744c57783ba96999c34a47cf99b18814571a1ed823a398efb26a0d79cc49becf"
   license "MIT"
 
   def install
